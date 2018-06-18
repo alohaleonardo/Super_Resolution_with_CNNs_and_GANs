ad1e55304e47c7486e064c6fc2d334161ba8362a
yiyang7 <yiyang7@stanford.edu>
2018-06-18 00:16:55 +0000

    add build dataset

--- build_dataset.py
+++ build_dataset.py
@@ -14,17 +14,14 @@ from skimage import data, color
 from skimage.transform import rescale, resize, downscale_local_mean
 from skimage import util
 
-INPUT_SIZE = 144
-OUTPUT_SIZE = 144
-
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='../img_align_celeba_test', help="Directory with the SIGNS dataset")
 parser.add_argument('--output_dir', default='../data/cnn_faces', help="Where to write the new data")
 parser.add_argument('--input_size', default='144', help="Where to write the new data")
 parser.add_argument('--output_size', default='144', help="Where to write the new data")
+parser.add_argument('--up_scale', default='4', help="Where to write the new data")
 
-
-def crop_and_save(filename, output_dir, out_size=OUTPUT_SIZE):
+def crop_and_save(filename, output_dir, out_size):
     """crop the image contained in `filename` and save it to the `output_dir`"""
     image = io.imread(filename)
     vert_start = (218 - out_size) // 2
@@ -34,7 +31,7 @@ def crop_and_save(filename, output_dir, out_size=OUTPUT_SIZE):
     cropped = image[vert_start:vert_end, horiz_start:horiz_end] # 218*178 -> 144*144
     io.imsave(os.path.join(output_dir, filename.split('/')[-1]), cropped)
     
-def blur_and_save(filename, output_dir, in_size=INPUT_SIZE, out_size=OUTPUT_SIZE):
+def blur_and_save(filename, output_dir, in_size, out_size, up_scale):
     """Blur the image contained in `filename` and save it to the `output_dir`"""
     image = io.imread(filename)
     vert_start = (218 - out_size) // 2
@@ -43,8 +40,8 @@ def blur_and_save(filename, output_dir, in_size=INPUT_SIZE, out_size=OUTPUT_SIZE
     horiz_end = horiz_start + out_size
     
     cropped = image[vert_start:vert_end, horiz_start:horiz_end] # 218*178 -> 144*144    
-    image_resized = resize(cropped, (out_size // 4, out_size // 4)) # upscaling factor 4
-    blur = resize(image_resized, (out_size, out_size)) # rescale back to 144 * 144
+    image_resized = resize(cropped, (out_size // up_scale, out_size // up_scale)) # upscaling factor 4
+    blur = resize(image_resized, (in_size, in_size)) # rescale back to 144 * 144
     io.imsave(os.path.join(output_dir, filename.split('/')[-1]), blur)
 
 if __name__ == '__main__':
@@ -56,6 +53,7 @@ if __name__ == '__main__':
     data_dir = args.data_dir
     INPUT_SIZE = int(args.input_size)
     OUTPUT_SIZE = int(args.output_size)
+    UP_SCALE = int(args.up_scale)
     
     # Get the filenames in data directory
     filenames = os.listdir(data_dir)
@@ -86,7 +84,6 @@ if __name__ == '__main__':
     else:
         print("Warning: output dir {} already exists".format(args.output_dir))
     
-    
     # Preprocess train, val and test
     for split in ['train', 'val', 'test']:
         # clear image
@@ -108,10 +105,10 @@ if __name__ == '__main__':
         print("Processing {} data, saving to {} and {}".format(split, output_dir_split_clear, output_dir_split_blur))
         # clear image
         for filename in tqdm(filenames[split]):
-            crop_and_save(filename, output_dir_split_clear, out_size=OUTPUT_SIZE)
+            crop_and_save(filename, output_dir_split_clear, OUTPUT_SIZE)
         #blur image
         for filename in tqdm(filenames[split]):
-            blur_and_save(filename, output_dir_split_blur, in_size=INPUT_SIZE, out_size=OUTPUT_SIZE)
+            blur_and_save(filename, output_dir_split_blur, INPUT_SIZE, OUTPUT_SIZE, UP_SCALE)
     
     print("Done building dataset")
     
